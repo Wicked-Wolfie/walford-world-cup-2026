c432acd95e6670cf08ebf30bbcabd02b1d4314a8
Wicked-Wolfie <david@walford.me.uk>
2026-06-18 13:31:47 +0100

    Update hide-admin-on-public.js

--- hide-admin-on-public.js
+++ hide-admin-on-public.js
@@ -14,6 +14,7 @@
 
     const matchScorers = document.getElementById("match-scorers-admin");
     const resultEditor = document.getElementById("results-editor-admin");
+    const oldAdminPanel = document.getElementById("adminPanel");
     const goldenBootAdmins = document.querySelectorAll(".gb-admin");
 
     if (matchScorers) {
@@ -24,6 +25,10 @@
       resultEditor.style.display = adminMode ? "" : "none";
     }
 
+    if (oldAdminPanel) {
+      oldAdminPanel.style.display = adminMode ? "" : "none";
+    }
+
     goldenBootAdmins.forEach(panel => {
       panel.style.display = adminMode ? "" : "none";
     });
@@ -31,8 +36,10 @@
 
   document.addEventListener("DOMContentLoaded", () => {
     haApply();
+    setTimeout(haApply, 500);
     setTimeout(haApply, 1500);
     setTimeout(haApply, 3500);
+    setTimeout(haApply, 7000);
     setInterval(haApply, 1000);
   });
 
