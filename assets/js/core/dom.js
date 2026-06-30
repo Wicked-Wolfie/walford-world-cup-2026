048afab3c5108672d2851824842b218af2acd40d
Wicked-Wolfie <david@walford.me.uk>
2026-06-30 09:22:02 +0100

    Update dom.js

--- assets/js/core/dom.js
+++ assets/js/core/dom.js
@@ -1,1 +1,46 @@
+"use strict";
 
+window.WC = window.WC || {};
+
+window.WC.dom = {
+  el(id) {
+    return document.getElementById(id);
+  },
+
+  q(selector, root = document) {
+    return root.querySelector(selector);
+  },
+
+  qa(selector, root = document) {
+    return Array.from(root.querySelectorAll(selector));
+  },
+
+  show(id) {
+    const node = this.el(id);
+    if (node) node.classList.remove("hidden");
+  },
+
+  hide(id) {
+    const node = this.el(id);
+    if (node) node.classList.add("hidden");
+  },
+
+  setText(id, value) {
+    const node = this.el(id);
+    if (node) node.textContent = value ?? "";
+  },
+
+  setHtml(id, value) {
+    const node = this.el(id);
+    if (node) node.innerHTML = value ?? "";
+  },
+
+  esc(value) {
+    return String(value ?? "")
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+  }
+};
