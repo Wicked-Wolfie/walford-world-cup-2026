9e67c30f72790b7e1ad6020ad868a79b82d50916
Wicked-Wolfie <david@walford.me.uk>
2026-06-30 09:36:41 +0100

    Update state.js

--- assets/js/core/state.js
+++ assets/js/core/state.js
@@ -1,1 +1,27 @@
+"use strict";
 
+window.WC = window.WC || {};
+
+window.WC.state = {
+  teams: [],
+  fixtures: [],
+  results: [],
+  players: [],
+  scorers: [],
+
+  set(key, value) {
+    this[key] = Array.isArray(value) ? value : [];
+  },
+
+  get(key) {
+    return Array.isArray(this[key]) ? this[key] : [];
+  },
+
+  reset() {
+    this.teams = [];
+    this.fixtures = [];
+    this.results = [];
+    this.players = [];
+    this.scorers = [];
+  }
+};
