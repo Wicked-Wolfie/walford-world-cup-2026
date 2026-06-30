0e06c6a5bd6b631f914bbd8ece2e0f4a8cdc7c9d
Wicked-Wolfie <david@walford.me.uk>
2026-06-30 09:38:03 +0100

    Update data.js

--- assets/js/core/data.js
+++ assets/js/core/data.js
@@ -1,1 +1,29 @@
+"use strict";
 
+window.WC = window.WC || {};
+
+window.WC.data = {
+  fallbackTeams() {
+    return Array.isArray(window.FALLBACK_TEAMS)
+      ? window.FALLBACK_TEAMS
+      : [];
+  },
+
+  fallbackFixtures() {
+    return Array.isArray(window.FALLBACK_FIXTURES)
+      ? window.FALLBACK_FIXTURES
+      : [];
+  },
+
+  teams() {
+    const list = this.fallbackTeams();
+    window.WC.state.set("teams", list);
+    return list;
+  },
+
+  fixtures() {
+    const list = this.fallbackFixtures();
+    window.WC.state.set("fixtures", list);
+    return list;
+  }
+};
