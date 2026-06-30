c1b88287b8f967dc08edc85d025b2b9e164a0db9
Wicked-Wolfie <david@walford.me.uk>
2026-06-30 09:39:17 +0100

    Update startup.js

--- assets/js/core/startup.js
+++ assets/js/core/startup.js
@@ -1,1 +1,19 @@
+"use strict";
 
+window.WC = window.WC || {};
+
+window.WC.startup = {
+  init() {
+    if (window.WC.data) {
+      window.WC.data.teams();
+      window.WC.data.fixtures();
+    }
+
+    console.log("WC V6 Core loaded", {
+      teams: window.WC.state.get("teams").length,
+      fixtures: window.WC.state.get("fixtures").length
+    });
+  }
+};
+
+window.WC.startup.init();
